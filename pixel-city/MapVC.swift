dc897b209f32dc6f8253f35fa92047dfd229c8b5
Sebastian Salamanca <sebastiansalamanca@aol.com>
2018-02-04 18:20:42 -0500

    center map.

--- pixel-city/MapVC.swift
+++ pixel-city/MapVC.swift
@@ -15,29 +15,37 @@ class MapVC: UIViewController {
     //OUtlets
     @IBOutlet weak var mapView: MKMapView!
     
+    // Vars
     var locationManager = CLLocationManager()
     let authorizationStatus = CLLocationManager.authorizationStatus()
-    let regionRadius: Double = 1000
+    let regionRadous:Double = 1000
     
+    // Actions
+   
+    
+    @IBAction func centerMapWasPressed(_ sender: Any) {
+        if authorizationStatus == .authorizedAlways || authorizationStatus == .authorizedWhenInUse {
+            centerMapOnUserLocation()
+        }
+        
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         mapView.delegate = self
         locationManager.delegate = self
         configureLocationServices()
     }
-    
-    @IBAction func centerMapBtnWasPressed(_ sender: Any) {
-        if authorizationStatus == .authorizedAlways || authorizationStatus == .authorizedWhenInUse {
-            centerMapOnUserLocation()
-        }
-    }
-    
 }
 
-extension MapVC: MKMapViewDelegate {
-    func centerMapOnUserLocation() {
-        guard let coordinate = locationManager.location?.coordinate else { return }
-        let coordinateRegion = MKCoordinateRegionMakeWithDistance(coordinate, regionRadius * 2.0, regionRadius * 2.0)
+
+extension MapVC: MKMapViewDelegate{
+    
+    func centerMapOnUserLocation(){
+        guard let coordinate = locationManager.location?.coordinate else {return}
+        
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(coordinate, regionRadous * 2.0, regionRadous * 2.0)
+        
         mapView.setRegion(coordinateRegion, animated: true)
     }
 }
@@ -55,7 +63,5 @@ extension MapVC: CLLocationManagerDelegate {
         centerMapOnUserLocation()
     }
 }
-
-
 
 
